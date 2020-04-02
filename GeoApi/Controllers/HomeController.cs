1d5e7a4f445dac5276b7c118f40e6f2389ebbfac
Hassan Habib <hassanhabib@live.com>
2020-04-01 18:36:49 -0700

    Add project files.

---
+++ GeoApi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) PiorSoft, LLC All rights reserved.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => 
+            Ok("Sorry Mario, the princess is in another castle!");
+    }
+}
