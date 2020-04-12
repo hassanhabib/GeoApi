4d53099441f8caeb4cce2981c1ecf8f57a9e98ac
Hassan Habib <hassanhabib@live.com>
2020-04-12 14:29:54 -0700

    Integrate EFxceptions

--- GeoApi/Brokers/Storage/StorageBroker.cs
+++ GeoApi/Brokers/Storage/StorageBroker.cs
@@ -2,11 +2,12 @@
 // Copyright (c) PiorSoft, LLC All rights reserved.
 // ---------------------------------------------------------------
 
+using EFxceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace GeoApi.Brokers.Storage
 {
-    public partial class StorageBroker : DbContext, IStorageBroker
+    public partial class StorageBroker : EFxceptionsContext, IStorageBroker
     {
         public StorageBroker(DbContextOptions<StorageBroker> options)
             : base(options) => this.Database.Migrate();
